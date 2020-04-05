4beac4cf896c7f3c1e83fbb23338196a5f830297
Pranav Vaidik Dhulipala <pranav.d1993@tamu.edu>
2020-04-05 02:56:07 -0400

    Added the image to array preprocessor. One more preprocessorr needs to be added that can load frames from a video file.

--- preprocessing_tools.py
+++ preprocessing_tools.py
@@ -1,6 +1,17 @@
 import numpy as np
 import os
 import cv2
+from tensorflow.keras.preprocessing.image import img_to_array
+
+class ImageToArrayPreprocessor:
+	def __init__(self, dataFormat = None)
+		# store the image data format
+		self.dataFormat = dataFormat
+		
+	def preprocess(self, image):
+		# apply Keras utility function to correctly rearrange the
+		# dimensions of the image
+		return img_to_array(image, data_format=self.dataFormat)	
 
 class SimplePreprocessor:
     def __init__(self, width, height, inter = cv2.INTER_AREA):
