20ce535f9453760fec1166976ffb2f4cb35c5b56
Pranav Vaidik Dhulipala <pranav.d1993@tamu.edu>
2020-04-09 02:30:00 -0400

    Added a new image preprocessor that is aware of the aspect ratio

--- preprocessing_tools.py
+++ preprocessing_tools.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import cv2
 from tensorflow.keras.preprocessing.image import img_to_array
+import imutils
 
 class ImageToArrayPreprocessor:
 	def __init__(self, dataFormat = None):
@@ -24,8 +25,40 @@ class SimplePreprocessor:
     def preprocess(self, image):
         # resize the image to a fixed size, ignoring the aspect ratio
         return cv2.resize(image, (self.width, self.height), interpolation = self.inter)
+
+class AspectAwarePreprocessor:
+	def __init__(self, width, height, inter = cv2.INTER_AREA):
+		# store target image dimensions and interpolation methods 
+        #for resizing
+        self.width = width
+        self.height = height
+        self.inter = inter
         
-        
+	def preprocess(self, image):
+		# grab the dimensions of the image and then initialize the 
+		#details to use when cropping
+		(h,w) = image.shape[:2]
+		dW = 0
+		dH = 0
+		
+		# if width is smaller than height, then resize along the width
+		# (i.e., along the smaller dimension) and then update the deltas
+		# to crop the height to the desired dimension
+		if w < h:
+			image = imutils.resize(image,width = self.width, inter = self.inter)
+			dH = int((image.shape[0] - self.height)/2.0)
+		# otherwise resize along height
+		else:
+			image = imutils.resize(image, height=self.height, inter=self.inter)
+			dW = int((image.shape[1] - self.width)/2.0)
+		
+		# now regrab the dimensions and perform the crop
+		(h,w) = image.shape[:2]
+		image = image[dH:h-dH, dW:w-dW]
+		
+		# now resize the image to required spatial dimensions
+		return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
+		
 class SimpleDatasetLoader:
     def __init__(self, preprocessors = None):
         # takes a list of preprocessors as inputs and stores them
