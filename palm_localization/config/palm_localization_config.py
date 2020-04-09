aede5e37e5c4ca122b8e299dc7058b5ba0d7a228
Pranav Vaidik Dhulipala <pranav.d1993@tamu.edu>
2020-04-09 17:30:49 -0400

    updating config files

--- palm_localization/config/palm_localization_config.py
+++ palm_localization/config/palm_localization_config.py
@@ -1,5 +1,5 @@
 # define paths to images directory for left hand
-IMAGES_PATH = "data/left"
+IMAGES_PATH = "../data/left"
 
 # Assign a number of test and validation images to measure the metrics
 NUM_CLASSES = 22
@@ -7,9 +7,9 @@ NUM_VAL_IMAGES = 1250*NUM_CLASSES # change this later
 NUM_TEST_IMAGES = 1250*NUM_CLASSES # change this later
 
 # define path for output training, validation and testing HDF5 files
-TRAIN_HDF5 = "data/hdf5/train.hdf5"
-VAL_HDF5 = "data/hdf5/val.hdf5"
-TEST_HDF5 = "data/hdf5/test.hdf5"
+TRAIN_HDF5 = "../data/hdf5/train.hdf5"
+VAL_HDF5 = "../data/hdf5/val.hdf5"
+TEST_HDF5 = "../data/hdf5/test.hdf5"
 
 # path to output model file
 MODEL_PATH = "output/palm_net.model"
