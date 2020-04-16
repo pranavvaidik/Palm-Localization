abd17d8f6e352a6454ce6bb1eead2429a821e80e
Pranav Vaidik Dhulipala <pranav.d1993@tamu.edu>
2020-04-16 19:57:24 -0400

    fixed indentations and other syntax errors

--- palm_localization/tools/datasets/video_predictor.py
+++ palm_localization/tools/datasets/video_predictor.py
@@ -4,39 +4,38 @@ class VideoPredictor:
 	def __init__(self, model, preprocessors=None):
 		self.model = model
 		self.preprocessors = preprocessors
-		
+
 		if self.preprocessors is None:
 			self.preprocessors = []
 			
-	def load(self, videoPaths):
-		
+	def load(self, videoPaths):	
 		# loop over each video
 		for video_path in videoPaths:
 			# get file path for JSON laebls for the video
 			json_file_path = video_path[:-4]+'_predicted.json'
-			
+
 			json_labels = []
-			
+
 			# read video file
-			video cv2.videoCapture(video_path)
-			
+			video = cv2.videoCapture(video_path)
+
 			# get video metadata information
 			frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 			fps = video.get(cv2.CAP_PROP_FPS)
 			duration = frame_count/fps
 			width  = video.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
 			height = video.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
-			
+
 			print("[INFO] processing file: ",video_path)
-			
+
 			# skip to next video if there is a problem opening the file
 			if (video.isOpened()== False):  
 				print("Error opening video file:",video_path)
 				continue
-			
+
 			frame_number=0
-			
-			
+
+
 			# Read until video is completed 
 			while(video.isOpened()): 
 				# videoture frame-by-frame 
