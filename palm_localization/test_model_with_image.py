80a2a110dd3624551afe93db84165147c462eff1
Pranav Vaidik Dhulipala <pranav.d1993@tamu.edu>
2020-04-20 01:46:26 -0400

    Added a new file for testing the model on sample images. Yet to finish it

---
+++ palm_localization/test_model_with_image.py
@@ -0,0 +1,53 @@
+# import packages
+from config import palm_localization_config as config
+from tools.preprocessing import ImageToArrayPreprocessor, SimplePreprocessor,MeanPreprocessor
+from tensorflow.keras.models import load_model
+import json
+import numpy as np
+import progressbar
+import os
+import argparse
+import cv2
+import pickle
+
+# construct argument parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--image",required=True, help="path to input image")
+args = vars(ap.parse_args())
+
+imagePath = args["image"]
+image = cv2.imread(imagePath)
+
+# Load RGB means for traiing set
+means = json.loads(open(config.DATASET_MEAN).read())
+
+# load label encoders
+f = open(config.OUTPUT_PATH+"/label_encoders.pkl","rb")
+le_left_, le_right_ = pickle.loads(f.read())
+f.close()
+
+
+# initialize preprocessors
+sp = SimplePreprocessor(224,224)
+mp = MeanPreprocessor(means["R"], means["G"], means["B"])
+iap = ImageToArrayPreprocessor()
+
+# load the pretrained network
+print("[INFO] loading model...")
+model = load_model(config.MODEL_PATH)
+
+# preprocess image
+for p in [sp,mp,iap]:
+	image = p.preprocess(image)
+
+tensor = np.expand_dims(image, axis=0)
+
+# predict labels
+left_output, right_output = model.predict(tensor)
+
+
+
+print(left_output, le_left.classes_(argmax(left_output)))
+print(rightt_output, le_right.classes_(argmax(right_output)))
+
+
